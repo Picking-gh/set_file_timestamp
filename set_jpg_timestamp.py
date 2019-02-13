8d8a886d79238d09d487e6a668f9dc091b197cf8
Picking <wangke@gddky.csg.cn>
2019-02-13 21:31:30 +0800

    1、区分从tag及文件名获取的时间戳，以小者修改文件的创建和修改时间。2、优化处理过程提示。

--- set_jpg_timestamp.py
+++ set_jpg_timestamp.py
@@ -21,68 +21,84 @@ def set_jpg_timestamp(path):
     tags = exifread.process_file(f, details=False)
     f.close()
 
-    new_timestamp = None
+    # new_ctimestamp1 for timestamp found in file tags, new_ctimestamp2 for timestamp found in file name.
+    new_ctimestamp1 = None
+    new_ctimestamp2 = None
 
     file_name = split(path)[1]
+    print(f' [{file_name}]')
 
     # 尝试获取'Image DateTime'的时间
     try:
-        new_timestamp = time.mktime(time.strptime(
+        new_ctimestamp1 = time.mktime(time.strptime(
             tags['Image DateTime'].values, '%Y:%m:%d %H:%M:%S'))
+        print('  └─Found "Image DateTime"', end='')
     except KeyError:
-        print(f'No Image DateTime in [{file_name}], try next method.')
+        print('  └─"Image DateTime" not found, try next method.')
     except ValueError:
-        print(
-            f'Image DateTime in [{file_name}] is not valid, try next method.')
+        print('  └─"Image DateTime" is not valid, try next method.')
 
     # 尝试获取'EXIF DateTimeOriginal'的时间
-    if not new_timestamp:
+    if not new_ctimestamp1:
         try:
-            new_timestamp = time.mktime(time.strptime(
+            new_ctimestamp1 = time.mktime(time.strptime(
                 tags['EXIF DateTimeOriginal'].values, '%Y:%m:%d %H:%M:%S'))
+            print('  └─Found "EXIF DateTimeOriginal"', end='')
         except KeyError:
-            print(
-                f'No EXIF DateTimeOriginal key in [{file_name}], try next method.')
+            print('  └─"EXIF DateTimeOriginal" not found, try next method.')
         except ValueError:
-            print(
-                f'Image DateTime in [{file_name}] is not valid, try next method.')
+            print('  └─"EXIF DateTimeOriginal" is not valid, try next method.')
 
     # 尝试获取'EXIF DateTimeDigitized'的时间
-    if not new_timestamp:
+    if not new_ctimestamp1:
         try:
-            new_timestamp = time.mktime(time.strptime(
+            new_ctimestamp1 = time.mktime(time.strptime(
                 tags['EXIF DateTimeDigitized'].values, '%Y:%m:%d %H:%M:%S'))
+            print('  └─Found "EXIF DateTimeDigitized"', end='')
         except KeyError:
-            print(
-                f'No EXIF DateTimeDigitized key in [{file_name}], try next method.')
+            print('  └─"EXIF DateTimeDigitized" not found, try next method.')
         except ValueError:
-            print(
-                f'Image DateTime in [{file_name}] is not valid, try next method.')
+            print('  └─"EXIF DateTimeDigitized" is not valid, try next method.')
 
     # 尝试从文件名获取时间，文件名需形如'XXX_YYYYmmdd_HHMMSS(_XXXXX).jp(e)g'
-    if not new_timestamp:
-        file_name_no_ext = splitext(split(path)[1])[0]
-        try:
-            time_strs = file_name_no_ext.split('_')[1:3]
-            new_timestamp = time.mktime(time.strptime(
-                ''.join(time_strs), '%Y%m%d%H%M%S'))
-        except ValueError:
+    file_name_no_ext = splitext(split(path)[1])[0]
+    try:
+        time_strs = file_name_no_ext.split('_')[1:3]
+        new_ctimestamp2 = time.mktime(time.strptime(
+            ''.join(time_strs), '%Y%m%d%H%M%S'))
+        if new_ctimestamp1:
+            print(' and file name contains timestamp', end='')
+        else:
+            print('  └─File name contains timestamp', end='')
+    except (ValueError, OverflowError):
+        if not new_ctimestamp1:
             print(
-                f'File name in [{file_name}] contains no valid time info, nothing to try.')
+                '  └─File name dose not match XXX_YYYYmmdd_HHMMSS(_XXXXX).jp(e)g format.')
 
-    if new_timestamp:
-        old_timestamp = getctime(path)
-        if abs(old_timestamp - new_timestamp) >= 1:
-            ctime = Time(new_timestamp)
+    if new_ctimestamp1 or new_ctimestamp2:
+        if new_ctimestamp1 is None:
+            new_ctimestamp = new_ctimestamp2
+        elif new_ctimestamp2 is None:
+            new_ctimestamp = new_ctimestamp1
+        else:
+            new_ctimestamp = new_ctimestamp1 if new_ctimestamp1 < new_ctimestamp2 else new_ctimestamp2
+        old_ctimestamp = getctime(path)
+
+        if abs(old_ctimestamp - new_ctimestamp) >= 1:
+            ctime = Time(new_ctimestamp)
             try:
                 handle = CreateFile(path, GENERIC_WRITE,
                                     FILE_SHARE_WRITE, None, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, None)
                 SetFileTime(handle, ctime, None, ctime)
                 CloseHandle(handle)
-                print('Done.')
+                print('. Done.')
             except:
                 print(
-                    f'Can not change properties of [{file_name}]. Make sure you have right permission. Skip it.')
+                    '. Can not change ctime and mtime. Make sure you have right permission. Skip it.')
+        else:
+            print('. No need to change. Skip it.')
+    else:
+        print('  └─No valid timestamp found. Skip it.')
 
 
 if __name__ == "__main__":
@@ -99,7 +115,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     for root, dirs, files in os.walk(path):
-        print(f'Processin {root}...')
+        print(f'Processin dir [{root}]')
         for a_file in files:
             if splitext(a_file)[1] in ('.jpg', '.jpeg'):
                 set_jpg_timestamp(join(root, a_file))
